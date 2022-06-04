f8fbf205a81f45c2638746b9924a17a36dc17da2
William05Cardozo <3784@holbertonschool.com>
2022-06-04 14:05:02 -0700

    New_Changes

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -11,14 +11,14 @@ $(document).ready(function () {
         $(".amenities > h4").text(Object.keys(dict_amenities).join(', '))
 
     });
+    $.ajax({
+        url: 'http://0.0.0.0:5001/api/v1/status/',
+        success: function (data) {
+            if (data.status === 'OK'){
+                $('#api_status').addClass('available');
+            } else {
+                $('#api_status').addClass('available');
+            }
+        }
+    });
 });
-
-/*
-$.get( "http://0.0.0.0:5001/api/v1/status/", function( data ) {
-  if (data.status === "OK") {
-      $("#api_status").addclass(".available")
-  }else{
-      $("#api_status").removeclass(".available")
-  }
-});
-*/
